b29526273671cc32e323b5fe75b05687e3554d6e
seejnn <44645578+seejnn@users.noreply.github.com>
2024-01-10 14:58:59 +0000

    add console log

--- scripts/programmers/programmers.js
+++ scripts/programmers/programmers.js
@@ -44,11 +44,13 @@ function getSolvedResult() {
 /* 파싱 직후 실행되는 함수 */
 async function beginUpload(bojData) {
   log('bojData', bojData);
+  console.log('bojData', bojData)
   if (isNotEmpty(bojData)) {
     startUpload();
 
     const stats = await getStats();
     const hook = await getHook();
+    console.log('hook', hook)
 
     const currentVersion = stats.version;
     /* 버전 차이가 발생하거나, 해당 hook에 대한 데이터가 없는 경우 localstorage의 Stats 값을 업데이트하고, version을 최신으로 변경한다 */
@@ -58,6 +60,8 @@ async function beginUpload(bojData) {
 
     /* 현재 제출하려는 소스코드가 기존 업로드한 내용과 같다면 중지 */
     cachedSHA = await getStatsSHAfromPath(`${hook}/${bojData.directory}/${bojData.fileName}`)
+    console.log('cachedSHA', cachedSHA)
+    console.log('bojData', bojData)
     calcSHA = calculateBlobSHA(bojData.code)
     log('cachedSHA', cachedSHA, 'calcSHA', calcSHA)
     if (cachedSHA == calcSHA) {
